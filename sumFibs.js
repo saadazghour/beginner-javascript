8a5b8b75931f0f1d4f853ac44a180c7ed08064ed
Azghour-Saad <saad.azghour@gmail.com>
2021-09-25 20:20:50 +0100

    Sum All Odd Fibonacci Numbers

---
+++ sumFibs.js
@@ -0,0 +1,26 @@
+function sumFibs(num) {
+ let fibSequance = [0, 1];
+
+ let counter =
+  fibSequance[fibSequance.length - 2] + fibSequance[fibSequance.length - 1];
+
+ while (counter <= num) {
+  fibSequance.push(counter);
+  counter =
+   fibSequance[fibSequance.length - 2] + fibSequance[fibSequance.length - 1];
+ }
+
+ let sumAllOdd = 0;
+
+ fibSequance.forEach(num => {
+  if (num % 2 !== 0) {
+   // console.log(num);
+   sumAllOdd += num;
+  }
+ });
+
+ //  console.log(sumAllOdd);
+ return sumAllOdd;
+}
+
+console.log(sumFibs(75025));
