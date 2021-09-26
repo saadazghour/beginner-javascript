13f726228bc7bc5bc3c7f8021546d0575dd4cf56
Azghour-Saad <saad.azghour@gmail.com>
2021-09-26 20:40:51 +0100

    dropElements, Drop it

---
+++ dropElements.js
@@ -0,0 +1,17 @@
+function dropElements(arr, func) {
+ let result = [];
+
+ for (let i = 0; i < arr.length; i++) {
+  if (func(arr[i])) {
+   return arr.slice(i);
+  }
+ }
+
+ return result;
+}
+
+let result = dropElements([1, 2, 3, 4], function(n) {
+ return n >= 3;
+});
+
+console.log(result);
