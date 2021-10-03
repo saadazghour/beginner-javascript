50748d8058fabf1edd334cd70faae1c3b7513d4e
Azghour-Saad <saad.azghour@gmail.com>
2021-10-03 15:37:39 +0100

    rewrit function

--- steamrollArray.js
+++ steamrollArray.js
@@ -16,3 +16,38 @@ function steamrollArray(arr) {
 let arr = steamrollArray([1, [2], [3, [[4]]]]);
 
 console.log(arr);
+
+// ------------------------------------------------------------
+
+// function steamrollArray(arr) {
+//  //  console.log(arr);
+//  let i = 0;
+//  while (i < 3) {
+//   arr = arr.flat();
+//   i++;
+//  }
+
+//  return arr;
+// }
+
+// ------------------------------------------------------------
+
+// function steamrollArray(arr) {
+//  while (arrayDataSet(arr)) {
+//   arr = arr.flat();
+//  }
+//  return arr;
+// }
+
+// function arrayDataSet(arr) {
+//  return arr.some(dataPoint => {
+//   return Array.isArray(dataPoint);
+//  });
+// }
+
+// [1, 2, 3, 4]
+// let arr = steamrollArray([1, [2], [3, [[4]]]]);
+
+// console.log(arr);
+
+// ------------------------------------------------------------
