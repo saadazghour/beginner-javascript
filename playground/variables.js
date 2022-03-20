a9823e24ef4ce83978f53fa08fa0872c888691bd
Azghour-Saad <saad.azghour@gmail.com>
2022-03-20 18:58:22 +0000

    Add Variables files!!

---
+++ playground/variables.js
@@ -0,0 +1,23 @@
+"use strict";
+
+// Can be updated
+var name = "Azghour";
+var strict = "Strict Mode !!";
+let country = true;
+
+name = "Fake";
+country = false;
+
+// you can't be updated
+const age = 28;
+
+if (age > 10) {
+  console.log("You are Old !!");
+}
+
+// CamelCase
+const iLovePizze = true;
+// UpperCamelCase
+const ILoveToEatHotDogs = false;
+// snake case !!
+const this_is_snake_case = false;
