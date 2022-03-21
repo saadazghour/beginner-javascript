b14be1daab3d5257ab770d8adb51711d42585c47
Azghour-Saad <saad.azghour@gmail.com>
2022-03-21 01:35:46 +0000

    Types Strings, in JS

---
+++ playground/types.js
@@ -0,0 +1,14 @@
+const lastName = "AZGHOUR";
+const middle = "middle";
+const firstName = "Saad";
+
+const concat = `My Name is ${firstName} and my last Name is ${lastName} !!`;
+
+const html = `
+  <div>
+    <h1>${lastName}</h1>
+    <h2>${firstName}</h2>
+  </div>
+`;
+
+// document.body.innerHTML = html;
