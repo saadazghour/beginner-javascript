934ae7b1cc13aa28d6411d855ec85e7044f8c10b
Azghour-Saad <saad.azghour@gmail.com>
2022-03-21 02:28:20 +0000

    Types Numbers!

--- playground/types.js
+++ playground/types.js
@@ -12,3 +12,16 @@ const html = `
 `;
 
 // document.body.innerHTML = html;
+
+const smarties = 20;
+const kids = 3;
+
+const eachKidsGet = smarties / kids;
+
+const exactGet = Math.floor(eachKidsGet);
+
+// console.log(`Each kids get ${exactGet}`);
+
+const dadGets = smarties % kids;
+
+// console.log(`Dad Gets ${dadGets}`);
